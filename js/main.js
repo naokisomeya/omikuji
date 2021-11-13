8649b8ae0d051c007e222ed232390d8127a83219
naokisomeya <sometimes.b0105@gmail.com>
2021-11-13 13:23:08 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,16 @@
+'user strict';
+
+{
+  const btn = document.getElementById('btn');
+
+  btn.addEventListener('click', () => {
+    const n = Math.random();
+    if (n < 0.05) {
+      btn.textContent = '大吉';
+    } else if (n < 0.2) {
+      btn.textContent = '中吉';
+    } else {
+      btn.textContent = '最凶'
+    }
+  });
+}
